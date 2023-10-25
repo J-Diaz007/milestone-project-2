c014f5083eece43c31d5f76fb485a12b8734f0fc
JDiaz <jdiaz_007@outlook.com>
2023-10-24 21:36:38 -0700

    updated code in app.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,30 @@
+import axios from "axios";
+import React, { useEffect, useState } from "react";
+
 function App() {
+  const [message, setMessage] = useState("Loading...");
+  const [secret, setSecret] = useState("Loading...");
+
+  const API_URL = process.env.REACT_APP_API_URL || "http://localhost:5000";
+
+  useEffect(() => {
+      axios.get(`${API_URL}/`).then((resp) => {
+          setMessage(resp.data.message);
+          setSecret(resp.data.secret);
+      });
+  }, []);
+
   return (
     <div className="App">
       <header className="App-header">
         <p><strong>Wazzzup??!! 🤪</strong></p>
         <p>Scotty</p>
         <p>Sasa</p>
+        <h3>{message}</h3>
+        <h4>Don't tell anyone this: {secret}</h4>
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
       </header>
     </div>
   );
