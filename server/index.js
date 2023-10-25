2db44e893c2c6e7e5c26278e80f5f1c1a237a6c7
JDiaz <jdiaz_007@outlook.com>
2023-10-24 20:54:35 -0700

    updated code

--- server/index.js
+++ server/index.js
@@ -12,10 +12,10 @@ const port = process.env.PORT || 5000;
 
 app.get("/", (req, res) => {
     res.send({
-        message: "Hello Devs!"
-        database: process.env.NOT_SO_SECRET
-    })
-})
+        message: "Hello Devs!",
+        secret: process.env.NOT_SO_SECRET,
+    });
+});
 
 app.listen(port, () => {
     console.log('Server is running on port ${port}')
