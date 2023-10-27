0b55e03cac6ef17b2a8a79c451fe5a3b1211cd93
scottyTwoskip <scottyeansor@gmail.com>
2023-10-27 10:10:53 -0700

    linked up textInput to the home component so it displays text on page. reset by refreshing(not stored via server yet)

--- client/src/Components/Home.jsx
+++ client/src/Components/Home.jsx
@@ -1,29 +1,25 @@
-import React, { useState } from 'react'
-import InputText from './InputText';
+import React, { useState } from "react";
+import InputText from "./InputText";
 
 function Home() {
-
-    const [posts, setPosts] = useState([]);
-
-    return (
+  const [posts, setPosts] = useState([]);
+  //created handleNewPost to add the previous item 'previousPosts + PostText'
+  const handleNewPost = (postText) => {
+    setPosts((prevPosts) => [postText, ...prevPosts]);
+  };
+  return (
+    <div>
+      <h1>Instagram</h1>
+      <InputText onNewPost={handleNewPost} />
+      {posts.length === 0 ? (
         <div>
-            <h1>Instagram</h1>
-            <InputText />
-            {
-                posts.length === 0
-                    ?
-                    <div><h2>Nothing Posted</h2></div>
-                    :
-                    posts.map(post => (
-                        <div>
-                            {post}
-                        </div>
-                    ))
-            }
+          <h2>Nothing Posted</h2>
         </div>
-
-    )
+      ) : (
+        posts.map((post, index) => <div key={index}>{post}</div>)
+      )}
+    </div>
+  );
 }
 
 export default Home;
-
--- client/src/Components/InputText.jsx
+++ client/src/Components/InputText.jsx
@@ -1,12 +1,37 @@
-import React from 'react'
+import React, { useState } from "react";
 
-function InputText() {
-    return (
-        <div>
-            <input type="text" name="" id="" />
-            <button type="submit">Submit</button>
-        </div>
-    )
+function InputText(props) {
+  const [text, setText] = useState("");
+  //handle submit creates a value for the text input then
+  //trim makes it so user cant just press spaces.
+  //made an alert that triggers if not text.
+
+  //also, added a form tag so we can submit using the enter button that is why it requires e.preventDefault()
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (text.trim() === "") {
+      alert("Please enter a valid caption!");
+    } else {
+      props.onNewPost(text);
+      setText("");
+    }
+  };
+  return (
+    <div>
+      {/* form tag added here */}
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          value={text}
+          onChange={(e) => setText(e.target.value)}
+          id="input-text"
+        />
+        <button type="submit" onClick={handleSubmit}>
+          Submit
+        </button>
+      </form>
+    </div>
+  );
 }
 
-export default InputText
+export default InputText;
