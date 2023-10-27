8a245f5fe2d2926eedf469e60c602b13f1fc66fb
JDiaz <jdiaz_007@outlook.com>
2023-10-27 08:12:26 -0700

    deleted code in app.jsx and readded a homepage and input text component

--- client/src/Components/TodoInput.jsx
+++
@@ -1,2 +0,0 @@
-import React, { UseState} from 'react';
-
---
+++ client/src/InputText.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function InputText() {
+    return (
+        <div>
+            <input type="text" name="" id="" />
+            <button type="submit">Submit</button>
+        </div>
+    )
+}
+
+export default InputText
