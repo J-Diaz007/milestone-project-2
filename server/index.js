11554e7c390e17fcd27d64d99085e926f0686bf4
scottyTwoskip <scottyeansor@gmail.com>
2023-10-31 20:21:01 -0700

    linked up to back end and posts shows up in compass

--- server/index.js
+++ server/index.js
@@ -1,22 +1,22 @@
 const express = require("express");
-const app = express();
 const cors = require("cors");
 const dotenv = require("dotenv");
-const mongoose = require("mongoose")
+const mongoose = require("mongoose");
 const Posts = require("./models/posts");
 
-// * Will load environment variables
+// Load environment variables
 dotenv.config();
 
+const app = express();
 app.use(cors());
-app.use(express.json())
+app.use(express.json());
 
 const port = process.env.PORT || 5000;
-const mongoUri = process.env.MONGO_URI || "mongodb://localhost:27017/instagram"
+const mongoUri = process.env.MONGO_URI || "mongodb://localhost:27017/instagram";
 
-app.get("/",(req,res)=>{
-    res.send("Hello Dev")
-});
+mongoose.connect(mongoUri, { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(() => console.log('Connected to MongoDB'))
+    .catch(err => console.error('Failed to connect to MongoDB:', err));
 
 app.post("/api/posts", (req, res) => {
     let newPost = { ...req.body };
@@ -28,9 +28,19 @@ app.post("/api/posts", (req, res) => {
     });
 });
 
+app.get("/api/posts", (req, res) => {
+    Posts.find({}, (err, posts) => {
+        if (err) return res.status(500).json({ error: err });
+        res.json(posts);
+    });
+});
 
-mongoose.connect(mongoUri,)
+// Error handling middleware
+app.use((err, req, res, next) => {
+    console.error(err.stack);
+    res.status(500).send('Something went wrong!');
+});
 
 app.listen(port, () => {
     console.log(`Server is running on port ${port}`);
-})
+});
