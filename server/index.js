744d90e615918e061d15c0343fff097e9a9ae532
scottyTwoskip <scottyeansor@gmail.com>
2023-10-31 20:24:06 -0700

    fixed posts to back end

--- server/index.js
+++ server/index.js
@@ -12,11 +12,11 @@ app.use(cors());
 app.use(express.json());
 
 const port = process.env.PORT || 5000;
-const mongoUri = process.env.MONGO_URI || "mongodb://localhost:27017/instagram"
+const mongoUri = process.env.MONGO_URI || "mongodb://localhost:27017/instagram";
 
-app.get("/", (req, res) => {
-    res.send("Hello Dev")
-});
+mongoose.connect(mongoUri, { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(() => console.log('Connected to MongoDB'))
+    .catch(err => console.error('Failed to connect to MongoDB:', err));
 
 app.post("/api/posts", (req, res) => {
     let newPost = { ...req.body };
@@ -28,9 +28,19 @@ app.post("/api/posts", (req, res) => {
     });
 });
 
+app.get("/api/posts", (req, res) => {
+    Posts.find({}, (err, posts) => {
+        if (err) return res.status(500).json({ error: err });
+        res.json(posts);
+    });
+});
 
-mongoose.connect(mongoUri,)
+// Error handling middleware
+app.use((err, req, res, next) => {
+    console.error(err.stack);
+    res.status(500).send('Something went wrong!');
+});
 
 app.listen(port, () => {
     console.log(`Server is running on port ${port}`);
-})
+});
