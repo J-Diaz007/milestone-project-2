56b8bfa8f3d87800a42679d4a3bc26479562f36f
Dragicevic09 <Sasadragicevic1995@gmail.com>
2023-10-31 20:19:30 -0700

    css update

--- server/index.js
+++ server/index.js
@@ -1,36 +1,43 @@
 const express = require("express");
-const app = express();
 const cors = require("cors");
 const dotenv = require("dotenv");
-const mongoose = require("mongoose")
-const Posts = require("./models/posts");
+const mongoose = require("mongoose");
 
-// * Will load environment variables
+// Load environment variables
 dotenv.config();
 
+const app = express();
 app.use(cors());
-app.use(express.json())
 
 const port = process.env.PORT || 5000;
-const mongoUri = process.env.MONGO_URI || "mongodb://localhost:27017/instagram"
+const mongoUri = process.env.MONGO_URI || "mongodb://localhost:27017/instagram";
 
-app.get("/",(req,res)=>{
-    res.send("Hello Dev")
-});
-
-app.post("/api/posts", (req, res) => {
-    let newPost = { ...req.body };
-    Posts.create(newPost, (err, post) => {
-        if (err) {
-            return res.status(500).json({ message: "Error creating post", error: err });
-        }
-        res.json(post);
+// Connect to MongoDB
+mongoose.connect(mongoUri, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => {
+    console.log("Connected to MongoDB");
+    // Start the server after successfully connecting to MongoDB
+    app.listen(port, () => {
+      console.log(`Server is running on port ${port}`);
     });
+  })
+  .catch(err => {
+    console.error("Could not connect to MongoDB", err);
+  });
+
+const sayHello = (req, res) => {
+  res.send({
+    message: "Hello Dev!",
+    secret: process.env.NOT_SO_SECRET,
+  });
+}
+
+// Define a route for the root path ("/") with the sayHello handler
+app.get("/", sayHello);
+
+// Define your other routes here
+app.post("/api/posts", (req, res) => {
+  // Define the logic for handling POST requests to /api/posts
 });
 
-
-mongoose.connect(mongoUri,)
-
-app.listen(port, () => {
-    console.log(`Server is running on port ${port}`);
-})
+// You can define more routes as needed
