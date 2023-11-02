efaa07307bc9f9838b0c94bd2f863a6fadee9776
JDiaz <jdiaz_007@outlook.com>
2023-11-01 18:39:37 -0700

    created update component

---
+++ client/src/Components/PostUpdate.jsx
@@ -0,0 +1,31 @@
+import React, { Component } from 'react';
+
+class PostUpdate extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      newName: '',
+    };
+  }
+
+  handleUpdatePost = () => {
+    const { index, onUpdate } = this.props;
+    onUpdate(index, this.state.newName);
+    this.setState({ newName: '' });
+  }
+
+  render() {
+    return (
+      <div>
+        <input
+          type="text"
+          value={this.state.newName}
+          onChange={(e) => this.setState({ newName: e.target.value })}
+        />
+        <button onClick={this.handleUpdatePost}>Update</button>
+      </div>
+    );
+  }
+}
+
+export default PostUpdate;
