032cf3db652395ca894faa246970b38c9d994042
JDiaz <jdiaz_007@outlook.com>
2023-11-05 09:24:28 -0800

    Revert "updated line 4 in posts.js file from imageUrl to imgUrl"
    This reverts commit 60e27d118a0427c40a709a108fd346c188b9fd7f.

--- server/models/posts.js
+++ server/models/posts.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose')
 
 let postsSchema = new mongoose.Schema({
-    imgUrl: { type: String, required: false },
+    imageUrl: { type: String, required: false },
     caption: { type: String, required: true }
 });
 
