df81d0170b938ca6206e99198cede5e3f4c4b177
JDiaz <jdiaz_007@outlook.com>
2023-11-05 10:36:52 -0800

    updated posts.js file

--- server/models/posts.js
+++ server/models/posts.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose')
 
 let postsSchema = new mongoose.Schema({
-    imageUrl: { type: String, required: false },
+    imgUrl: { type: String, required: false },
     caption: { type: String, required: true }
 });
 
