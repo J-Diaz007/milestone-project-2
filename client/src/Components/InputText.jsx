71a0409e96420703ec2e6bbe1616289c5b3ae216
JDiaz <jdiaz_007@outlook.com>
2023-11-05 09:37:35 -0800

    created state for imageLoadError

--- client/src/Components/InputText.jsx
+++ client/src/Components/InputText.jsx
@@ -5,6 +5,7 @@ import axios from "axios";
 function InputText(props) {
   const [text, setText] = useState("");
   const [imgUrl, setImgUrl] = useState("");
+  const [imageLoadError, setImageLoadError] = useState(false);
 
   const isValidHttpUrl = (string) => {
     let url;
@@ -16,10 +17,14 @@ function InputText(props) {
     return url.protocol === "http:" || url.protocol === "https:";
   };
 
+  const handleImageError = () => {
+    setImageLoadError(true);
+  };
+
   const handleSubmit = (e) => {
     e.preventDefault();
-    if (text.trim() === "") {
-      alert("Please enter a valid post.");
+    if (text.trim() === "" && !imgUrl) {
+      alert("Please enter a valid caption or image URL.");
       return;
     }
     if (imgUrl && !isValidHttpUrl(imgUrl)) {
@@ -36,6 +41,7 @@ function InputText(props) {
         props.onNewPost(response.data);
         setText("");
         setImgUrl("");
+        setImageLoadError(false);
       })
       .catch((error) => {
         console.error("There was an error posting the caption:", error);
@@ -52,20 +58,24 @@ function InputText(props) {
             value={imgUrl}
             onChange={(e) => setImgUrl(e.target.value)}
             className="form-control"
-            placeholder="Enter image URL"
+            placeholder="Paste image URL here."
             aria-label="Image URL"
           />
           {imgUrl && isValidHttpUrl(imgUrl) && (
             <div className="img-preview" style={{ margin: "10px 0" }}>
-              <img
-                src={imgUrl}
-                alt="Preview"
-                style={{ maxWidth: "100%", height: "auto", display: "block" }}
-              />
+              {imageLoadError ? (
+                <div>Error loading image</div>
+              ) : (
+                <img
+                  src={imgUrl}
+                  alt="Image Preview"
+                  style={{ maxWidth: "100%", height: "auto", display: "block" }}
+                  onError={handleImageError}
+                />
+              )}
             </div>
           )}
           <input
-            required
             type="text"
             value={text}
             onChange={(e) => setText(e.target.value)}
